afdbe797cbdc12494687dbb8d00395960246df67
BeatrizAlmeida03 <beatriz_almeida11@hotmail.com>
2023-01-07 04:17:45 +0000

    Some comments for doxy

--- src/Gestor.h
+++ src/Gestor.h
@@ -13,9 +13,15 @@
 
 class Gestor {
     public:
+        /// Default construtor.
         Gestor();
+        /// Reads the contents of the file "airlines.csv" and place them in a vector.
         void read_airlines();
+        /// Reads the contents of the file "airports.csv" and place them in a vector.
+        /// \param graph
         void read_airports(Graph& graph);
+        /// Getter.
+        /// \return Unordered set containing airlines
         unordered_set<Airline, AirlineHash> getAirlines();
 
     private:
